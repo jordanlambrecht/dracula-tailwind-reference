4d58ff432877bc41495d20a73163dca5c097c9ec
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 14:51:15 -0600

    feat: added plausible functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import PlausibleProvider from "next-plausible"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain='dracula-tailwind.com'>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import { Analytics } from "@vercel/analytics/react"
-;<Analytics />
+
 export default function Document() {
   return (
     <Html lang='en' className='darker'>
@@ -9,7 +8,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <Analytics />
     </Html>
   )
 }
