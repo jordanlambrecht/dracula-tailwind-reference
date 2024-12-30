991f15ef649c39c4d54536946c257f6b60e0ed1b
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 15:32:36 -0600

    test: plausible debugging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       domain='dracula-tailwind.com'
       trackOutboundLinks
       trackLocalhost
+      customDomain={"https://analytics.jordy.world"}
       selfHosted
       enabled
     >
