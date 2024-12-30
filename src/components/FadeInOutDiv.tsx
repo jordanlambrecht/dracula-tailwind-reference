1720e6d4bd80e39ce5139f72f576553bc98f8621
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 14:50:18 -0600

    style: added tailwind keyframes

--- src/components/FadeInOutDiv.tsx
+++ src/components/FadeInOutDiv.tsx
@@ -1,48 +1,32 @@
 import React, { useState, useEffect } from "react"
-import styled, { keyframes } from "styled-components"
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`
-
-const fadeOut = keyframes`
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-`
-
-const StyledDiv = styled.div<{ isVisible: boolean }>`
-  animation: ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)} 0.5s
-    ease-in-out forwards;
-`
+import classNames from "classnames"
 
 interface FadeInOutDivProps {
   isVisible: boolean
   children: React.ReactNode
 }
 
-const FadeInOutDiv: React.FC<FadeInOutDivProps> = ({ isVisible, children }) => {
+const FadeInOutDiv = ({ isVisible, children }: FadeInOutDivProps) => {
   const [show, setShow] = useState(isVisible)
 
   useEffect(() => {
     if (isVisible) {
       setShow(true)
     } else {
-      const timeout = setTimeout(() => setShow(false), 500) // Match this duration with animation duration
+      const timeout = setTimeout(() => setShow(false), 500)
       return () => clearTimeout(timeout)
     }
   }, [isVisible])
 
   return show || isVisible ? (
-    <StyledDiv isVisible={isVisible}>{children}</StyledDiv>
+    <div
+      className={classNames(
+        "transition-opacity duration-500",
+        isVisible ? "animate-fadeIn" : "animate-fadeOut"
+      )}
+    >
+      {children}
+    </div>
   ) : null
 }
 
