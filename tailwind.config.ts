5ef8726e88a407bf499fd3d8f41826aa01a4fb1a
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 14:49:43 -0600

    style: added new tailwind keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out forwards",
+        fadeOut: "fadeOut 0.5s ease-in-out forwards",
+      },
+
       maxWidth: {
         "8xl": "96rem",
       },
