bce85a0c3760f6e40989992256cd7992c0df365f
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 14:50:40 -0600

    refactor: rewrote how cards appear

--- src/components/DraculaCard.tsx
+++ src/components/DraculaCard.tsx
@@ -1,35 +1,19 @@
-import React, { useEffect, useState } from "react"
-import classNames from "classnames"
-import { H1, H2, H3, Subheader } from "@/components/typography/typography"
+import { useEffect, useState } from "react"
+import { H3 } from "@/components/typography/typography"
 import FadeInOutDiv from "@/components/FadeInOutDiv"
 import hexRgb from "hex-rgb"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Icon_Copy from "@/components/icons/Icon_Copy"
+
 interface DraculaCardProps {
-  className?: string
   color: string
   shade: string
   hex: string
 }
 
-const DraculaCard: React.FC<DraculaCardProps> = ({
-  className,
-  color,
-  shade,
-  hex,
-}) => {
-  const defaultClasses = "w-full h-full aspect-square rounded"
-  const combinedClasses = classNames(defaultClasses, className)
-
+const DraculaCard = ({ color, shade, hex }: DraculaCardProps) => {
   const [isCopied, setIsCopied] = useState<boolean | false>(false)
 
-  const CopyButton = () => {
-    return (
-      <button className='mr-1 duration-150 hover:scale-105 text-dracula-darker-500 hover:text-dracula-green'>
-        <Icon_Copy className='w-4 -mb-1' />
-      </button>
-    )
-  }
   useEffect(() => {
     if (isCopied) {
       const timer = setTimeout(() => {
@@ -43,36 +27,38 @@ const DraculaCard: React.FC<DraculaCardProps> = ({
 
   const lastWord = color.trim().split(" ").pop()
   return (
-    <div className='flex flex-col w-auto h-full p-2 rounded-lg shadow-sm snap-start bg-blue-50'>
-      <div className={combinedClasses} style={{ backgroundColor: hex }}>
-        <FadeInOutDiv isVisible={isCopied}>
-          <div className='inline-block w-auto p-2 m-4 text-sm rounded shadow-sm bg-dracula text-dracula-light'>
-            📄 Copied!
-          </div>
-        </FadeInOutDiv>
-      </div>
-      <div className='flex flex-col justify-between w-full px-2 pt-2 h-1/3'>
+    <div className='relative flex flex-col justify-between p-2 w-48 md:w-64 rounded-lg shadow-sm snap-start bg-blue-50'>
+      <FadeInOutDiv isVisible={isCopied}>
+        <div className='absolute top-6 left-6 w-auto px-4 py-2 text-sm  bg-dracula text-dracula-light font-semibold rounded shadow'>
+          📄 Copied!
+        </div>
+      </FadeInOutDiv>
+
+      <div
+        className='w-full aspect-square'
+        style={{ backgroundColor: hex }}
+      ></div>
+
+      <div className='flex-shrink w-full px-2 pt-2 flex-nowrap text-nowrap'>
         <H3 className='leading-none'>
-          {shade != "DEFAULT" ? `${lastWord}-${shade}` : shade}
+          {shade !== "DEFAULT" ? `${lastWord}-${shade}` : shade}
         </H3>
 
-        <div className='flex flex-row justify-between text-sm text-dracula'>
-          <div className='flex flex-row items-center'>
-            <CopyToClipboard text={hex} onCopy={() => setIsCopied(true)}>
-              <span>
-                <CopyButton />
-              </span>
-            </CopyToClipboard>
-            {hex}
-          </div>
-          <div className='flex flex-row items-center '>
-            <CopyToClipboard text={rgb} onCopy={() => setIsCopied(true)}>
-              <span>
-                <CopyButton />
-              </span>
-            </CopyToClipboard>
-            <span className='py-0 my-0 leading-none'>{rgb}</span>
-          </div>
+        <div>
+          <CopyToClipboard text={hex} onCopy={() => setIsCopied(true)}>
+            <button className='inline mr-1 duration-150 hover:scale-105 text-dracula-darker-500 hover:text-dracula-green'>
+              <Icon_Copy className='w-4 -mb-1 inline-block' />
+              <span className='ml-1 text-sm py-0 my-0 leading-none'>{hex}</span>
+            </button>
+          </CopyToClipboard>
+        </div>
+        <div>
+          <CopyToClipboard text={rgb} onCopy={() => setIsCopied(true)}>
+            <button className='inline mr-1 duration-150 hover:scale-105 text-dracula-darker-500 hover:text-dracula-green'>
+              <Icon_Copy className='w-4 -mb-1 inline-block' />
+              <span className='ml-1 text-sm py-0 my-0 leading-none'>{rgb}</span>
+            </button>
+          </CopyToClipboard>
         </div>
       </div>
     </div>
